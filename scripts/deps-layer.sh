6d41aeafe1ede13dcf285011bbed1d93d6b1237c
lukehedger <lh4@pm.me>
2020-03-27 19:35:34 +0000

    Move Lambda Layer node_modules to nodejs dir

--- scripts/deps-layer.sh
+++ scripts/deps-layer.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 
 # Install Yarn workspace dependencies to Lambda Layer directory
-yarn install --flat --modules-folder lambda-layer/node_modules --no-progresss --non-interactive --production --silent
+yarn install --flat --modules-folder lambda-layer/nodejs/node_modules --no-progresss --non-interactive --production --silent
 
 # Remove workspace directories from Lambda Layer node_modules
 # to prevent circular references
 rm -rf \
-  lambda-layer/node_modules/cloud-infrastructure \
-  lambda-layer/node_modules/lambda-a \
-  lambda-layer/node_modules/lambda-b
+  lambda-layer/nodejs/node_modules/cloud-infrastructure \
+  lambda-layer/nodejs/node_modules/lambda-a \
+  lambda-layer/nodejs/node_modules/lambda-b
