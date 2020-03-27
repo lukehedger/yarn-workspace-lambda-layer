0f2d562d5103d71ea19599576859d111f96c5d99
lukehedger <lh4@pm.me>
2020-03-27 19:26:18 +0000

    Add dependencies Lambda Layer

--- cloud-infrastructure/lib/infrastructure-stack.ts
+++ cloud-infrastructure/lib/infrastructure-stack.ts
@@ -1,14 +1,20 @@
-import { Code, Function, Runtime } from "@aws-cdk/aws-lambda";
+import { Code, Function, LayerVersion, Runtime } from "@aws-cdk/aws-lambda";
 import { Construct, Stack, StackProps } from "@aws-cdk/core";
 
 export class InfrastructureStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
+    const dependenciesLayer = new LayerVersion(this, "DependenciesLayer", {
+      code: Code.fromAsset("../lambda-layer"),
+      compatibleRuntimes: [Runtime.NODEJS_12_X]
+    });
+
     new Function(this, "ALambda", {
       code: Code.fromAsset("../lambda-a/bin"),
       functionName: "A",
       handler: "a.handler",
+      layers: [dependenciesLayer],
       runtime: Runtime.NODEJS_12_X
     });
 
@@ -16,6 +22,7 @@ export class InfrastructureStack extends Stack {
       code: Code.fromAsset("../lambda-b/bin"),
       functionName: "B",
       handler: "b.handler",
+      layers: [dependenciesLayer],
       runtime: Runtime.NODEJS_12_X
     });
   }
