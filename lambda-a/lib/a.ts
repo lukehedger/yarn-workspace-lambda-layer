4cc58221ebede9e963acf40be6a8639d7f18ee2f
lukehedger <lh4@pm.me>
2020-03-28 19:11:44 +0000

    Add common utility example

--- lambda-a/lib/a.ts
+++ lambda-a/lib/a.ts
@@ -1,8 +1,11 @@
 import { Handler } from "aws-lambda";
 import { addDays } from "date-fns";
+import { logger } from "/opt/nodejs/util-logger";
 
 export const handler: Handler = async () => {
   const tomorrow = addDays(new Date(), 1);
+
+  logger(tomorrow);
 
   return {
     tomorrow: tomorrow
--- scripts/deps-layer.sh
+++ scripts/deps-layer.sh
@@ -8,4 +8,8 @@ yarn install --flat --modules-folder lambda-layer/nodejs/node_modules --no-progr
 rm -rf \
   lambda-layer/nodejs/node_modules/cloud-infrastructure \
   lambda-layer/nodejs/node_modules/lambda-a \
-  lambda-layer/nodejs/node_modules/lambda-b
+  lambda-layer/nodejs/node_modules/lambda-b \
+  lambda-layer/nodejs/node_modules/util-logger
+
+# Copy common utilities
+cp -r util-logger/bin lambda-layer/nodejs/util-logger
